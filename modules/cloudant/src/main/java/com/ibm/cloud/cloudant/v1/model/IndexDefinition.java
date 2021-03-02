90c711289600426404c111e542356a1b0f9e4535
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2021-03-02 14:50:54 +0000

    Update generated source code (#98)
    Generated SDK source code using:
    - Generator version 3.28.0
    - Specification version 1.0.0-dev0.0.32
    - Automation (cloudant-sdks) version 279239e

--- modules/cloudant/src/main/java/com/ibm/cloud/cloudant/v1/model/IndexDefinition.java
+++ modules/cloudant/src/main/java/com/ibm/cloud/cloudant/v1/model/IndexDefinition.java
@@ -14,6 +14,7 @@ package com.ibm.cloud.cloudant.v1.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.annotations.SerializedName;
 import com.ibm.cloud.sdk.core.service.model.GenericModel;
@@ -33,6 +34,8 @@ public class IndexDefinition extends GenericModel {
   protected List<IndexField> fields;
   @SerializedName("index_array_lengths")
   protected Boolean indexArrayLengths;
+  @SerializedName("partial_filter_selector")
+  protected Map<String, Object> partialFilterSelector;
 
   /**
    * Builder.
@@ -42,12 +45,14 @@ public class IndexDefinition extends GenericModel {
     private IndexTextOperatorDefaultField defaultField;
     private List<IndexField> fields;
     private Boolean indexArrayLengths;
+    private Map<String, Object> partialFilterSelector;
 
     private Builder(IndexDefinition indexDefinition) {
       this.defaultAnalyzer = indexDefinition.defaultAnalyzer;
       this.defaultField = indexDefinition.defaultField;
       this.fields = indexDefinition.fields;
       this.indexArrayLengths = indexDefinition.indexArrayLengths;
+      this.partialFilterSelector = indexDefinition.partialFilterSelector;
     }
 
     /**
@@ -125,6 +130,17 @@ public class IndexDefinition extends GenericModel {
       this.indexArrayLengths = indexArrayLengths;
       return this;
     }
+
+    /**
+     * Set the partialFilterSelector.
+     *
+     * @param partialFilterSelector the partialFilterSelector
+     * @return the IndexDefinition builder
+     */
+    public Builder partialFilterSelector(Map<String, Object> partialFilterSelector) {
+      this.partialFilterSelector = partialFilterSelector;
+      return this;
+    }
   }
 
   protected IndexDefinition(Builder builder) {
@@ -132,6 +148,7 @@ public class IndexDefinition extends GenericModel {
     defaultField = builder.defaultField;
     fields = builder.fields;
     indexArrayLengths = builder.indexArrayLengths;
+    partialFilterSelector = builder.partialFilterSelector;
   }
 
   /**
@@ -190,6 +207,39 @@ public class IndexDefinition extends GenericModel {
    */
   public Boolean indexArrayLengths() {
     return indexArrayLengths;
+  }
+
+  /**
+   * Gets the partialFilterSelector.
+   *
+   * JSON object describing criteria used to select documents. The selector specifies fields in the document, and
+   * provides an expression to evaluate with the field content or other data.
+   *
+   * The selector object must:
+   *   * Be structured as valid JSON.
+   *   * Contain a valid query expression.
+   *
+   * Using a selector is significantly more efficient than using a JavaScript filter function, and is the recommended
+   * option if filtering on document attributes only.
+   *
+   * Elementary selector syntax requires you to specify one or more fields, and the corresponding values required for
+   * those fields. You can create more complex selector expressions by combining operators.
+   *
+   * Operators are identified by the use of a dollar sign `$` prefix in the name field.
+   *
+   * There are two core types of operators in the selector syntax:
+   * * Combination operators: applied at the topmost level of selection. They are used to combine selectors. In addition
+   * to the common boolean operators (`$and`, `$or`, `$not`, `$nor`) there are three combination operators: `$all`,
+   * `$elemMatch`, and `$allMatch`. A combination operator takes a single argument. The argument is either another
+   * selector, or an array of selectors.
+   * * Condition operators: are specific to a field, and are used to evaluate the value stored in that field. For
+   * instance, the basic `$eq` operator matches when the specified field contains a value that is equal to the supplied
+   * argument.
+   *
+   * @return the partialFilterSelector
+   */
+  public Map<String, Object> partialFilterSelector() {
+    return partialFilterSelector;
   }
 }
 
