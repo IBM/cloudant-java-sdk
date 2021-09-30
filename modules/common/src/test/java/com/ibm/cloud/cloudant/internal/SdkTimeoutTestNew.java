9caad49e953c166c6388915030fdb2930a6faf52
Veronika Matyus <veronika.matyus@ibm.com>
2021-09-30 19:08:43 +0200

    Refactor - introduced Helper functions

--- modules/common/src/test/java/com/ibm/cloud/cloudant/internal/SdkTimeoutTestNew.java
+++ modules/common/src/test/java/com/ibm/cloud/cloudant/internal/SdkTimeoutTestNew.java
@@ -15,6 +15,7 @@ package com.ibm.cloud.cloudant.internal;
 
 import com.ibm.cloud.cloudant.security.CouchDbSessionAuthenticator;
 import com.ibm.cloud.sdk.core.http.ServiceCall;
+import com.ibm.cloud.sdk.core.security.Authenticator;
 import com.ibm.cloud.sdk.core.security.BasicAuthenticator;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.cloud.sdk.core.security.NoAuthAuthenticator;
@@ -24,130 +25,69 @@ import okhttp3.Request;
 import okhttp3.internal.connection.RealCall;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
 
+// Every method tests an authenticator.
 public class SdkTimeoutTestNew {
     final long defaultTimeoutValue = 150L;
     final long authenticationTimeoutValue = 90L;
     final long customTimeoutValue = 10L;
 
-    // Every test case tests an authenticator.
     final long testCases[] = {
-            // 1. case: its default timeout value checked,
+            // 1. case: check default timeout value
             defaultTimeoutValue,
-            // 2. case: a custom overwrite possibility is checked.
+            // 2. case: check custom timeout overwrite
             customTimeoutValue
     };
 
-    @Test
-    void testNoAuthTimeout() throws NoSuchFieldException, IllegalAccessException {
-        CloudantBaseService myService = new CloudantBaseService(null, new NoAuthAuthenticator()) {};
-        myService.setServiceUrl("https://cloudant.example");
+    public static MockWebServer mockedServer;
 
-        // Create a server request for testing
-        HttpUrl requestUrl = HttpUrl.parse(myService.getServiceUrl()).newBuilder().addPathSegment("/").build();
-        Request req = new Request.Builder().url(requestUrl).get().build();
-
-        for (int i = 0; i < 2; i++) {
-            if (i == 1) {
-                // Change timeout
-                OkHttpClient client = myService.getClient();
-                client = client.newBuilder().readTimeout(testCases[i], TimeUnit.SECONDS).build();
-                myService.setClient(client);
-            }
-            // Create a service call for testing
-            ServiceCall<RealCall> serviceCall = myService.createServiceCall(req, null);
-            Field callField = serviceCall.getClass().getDeclaredField("call");
-            callField.setAccessible(true);
-            RealCall requestCall = (RealCall) callField.get(serviceCall);
-
-            assertEquals(TimeUnit.MILLISECONDS.toSeconds(requestCall.getClient().readTimeoutMillis()), testCases[i]);
-        }
+    @BeforeMethod
+    static void setUp() throws IOException {
+        mockedServer = new MockWebServer();
+        mockedServer.start();
     }
 
-    @Test
-    void testBasicAuthTimeout() throws NoSuchFieldException, IllegalAccessException {
-        BasicAuthenticator basicAuth = new BasicAuthenticator.Builder()
-                .username("user")
-                .password("psw")
-                .build();
-        CloudantBaseService myService = new CloudantBaseService(null, basicAuth) {};
-        myService.setServiceUrl("https://cloudant.example");
-
-        // Create a server request for testing
-        HttpUrl requestUrl = HttpUrl.parse(myService.getServiceUrl()).newBuilder().addPathSegment("/").build();
-        Request req = new Request.Builder().url(requestUrl).get().build();
-
-        for (int i = 0; i < 2; i++) {
-            if (i == 1) {
-                // Change timeout
-                OkHttpClient client = myService.getClient();
-                client = client.newBuilder().readTimeout(testCases[i], TimeUnit.SECONDS).build();
-                myService.setClient(client);
-            }
-            // Create a service call for testing
-            ServiceCall<RealCall> serviceCall = myService.createServiceCall(req, null);
-            Field callField = serviceCall.getClass().getDeclaredField("call");
-            callField.setAccessible(true);
-            RealCall requestCall = (RealCall) callField.get(serviceCall);
-
-            assertEquals(TimeUnit.MILLISECONDS.toSeconds(requestCall.getClient().readTimeoutMillis()), testCases[i]);
-        }
+    @AfterMethod
+    static void tearDown() throws IOException {
+        mockedServer.shutdown();
+    }
+    
+    // Helper functions
+    private CloudantBaseService createCloudantBaseService(Authenticator authInstance, String urlAsString) {
+        CloudantBaseService myService = new CloudantBaseService(null, authInstance) {};
+        myService.setServiceUrl(urlAsString);
+        return myService;
     }
 
-    @Test
-    void testSessionAuthTimeout() throws Exception {
-        CouchDbSessionAuthenticator sessionAuth = (CouchDbSessionAuthenticator)CouchDbSessionAuthenticator.newAuthenticator("user", "pass");
-        CloudantBaseService myService = new CloudantBaseService(null, sessionAuth) {};
-        myService.setServiceUrl("https://cloudant.example");
-
-        // Create a server request for testing
+    private Request createTestRequest(CloudantBaseService myService) {
         HttpUrl requestUrl = HttpUrl.parse(myService.getServiceUrl()).newBuilder().addPathSegment("/").build();
-        Request req = new Request.Builder().url(requestUrl).get().build();
+        return new Request.Builder().url(requestUrl).get().build();
+    }
 
-        MockWebServer server = new MockWebServer();
-        server.start();
-        String mockSession = "AuthSession=ABC123456DE";
-        String mockSetCookieValue = String.format("%s; Version=1; Expires=Thu, 09-Apr-2020 " +
-                "10:30:47 GMT; Max-Age=600; Path=/; HttpOnly", mockSession);
-        String mockSessionPostResponseBody = "{\"ok\":true,\"name\":\"testuser\"," +
-                "\"roles\":[\"admin\"]}";
-        MockResponse sessionPostResponse = new MockResponse()
-                .addHeader("Set-Cookie", mockSetCookieValue)
-                .setBody(mockSessionPostResponseBody);
+    private void setCustomTimeout(CloudantBaseService myService, long testCase) {
+        OkHttpClient client = myService.getClient();
+        client = client.newBuilder().readTimeout(testCase, TimeUnit.SECONDS).build();
+        myService.setClient(client);
+    }
 
-        server.enqueue(sessionPostResponse);
-
-        myService.setServiceUrl("http://" + server.getHostName() + ":" + server.getPort());
-
-        // Create a service call for testing
-        ServiceCall<RealCall> serviceCall = myService.createServiceCall(req, null);
+    private RealCall createServiceCall(CloudantBaseService serviceClient, Request serviceRequest) throws NoSuchFieldException, IllegalAccessException {
+        ServiceCall<RealCall> serviceCall = serviceClient.createServiceCall(serviceRequest, null);
         Field callField = serviceCall.getClass().getDeclaredField("call");
         callField.setAccessible(true);
-        RealCall requestCall = (RealCall) callField.get(serviceCall);
-        assertEquals(TimeUnit.MILLISECONDS.toSeconds(requestCall.getClient().readTimeoutMillis()), testCases[0]);
-
-        CouchDbSessionAuthenticator auth = (CouchDbSessionAuthenticator) myService.getAuthenticator();
-        Field clientField = auth.getClass().getDeclaredField("client");
-        clientField.setAccessible(true);
-        OkHttpClient c = (OkHttpClient) clientField.get(auth);
-        assertEquals(TimeUnit.MILLISECONDS.toSeconds(c.readTimeoutMillis()), authenticationTimeoutValue);
-
-        server.shutdown();
+        return (RealCall) callField.get(serviceCall);
     }
 
-    @Test
-    void testIAMAuthTimeout() throws Exception {
-        // Create auth server
-        MockWebServer server = new MockWebServer();
-        server.start();
-
+    private void setupIamAuthResponse() {
         String tokenResponse ="{\n" +
                 "  \"access_token\": \"abcd-1234\",\n" +
                 "  \"refresh_token\": \"00000000\",\n" +
@@ -160,26 +100,89 @@ public class SdkTimeoutTestNew {
                 .setResponseCode(200)
                 .setBody(tokenResponse);
 
-        server.enqueue(iamPostResponse);
+        mockedServer.enqueue(iamPostResponse);
+    }
 
+    private void setupSessionAuthRespones() {
+        String mockSetCookieValue = String.format("%s; Version=1; Expires=Thu, 09-Apr-2020 " +
+                "10:30:47 GMT; Max-Age=600; Path=/; HttpOnly", "AuthSession=ABC123456DE");
+        String mockSessionPostResponseBody = "{\"ok\":true,\"name\":\"testuser\"," +
+                "\"roles\":[\"admin\"]}";
+        MockResponse sessionPostResponse = new MockResponse()
+                .addHeader("Set-Cookie", mockSetCookieValue)
+                .setBody(mockSessionPostResponseBody);
+
+        mockedServer.enqueue(sessionPostResponse);
+    }
+
+    // Test implementations
+    @Test
+    void testNoAuthTimeout() throws NoSuchFieldException, IllegalAccessException {
+        NoAuthAuthenticator noAuth = new NoAuthAuthenticator();
+        CloudantBaseService myService = createCloudantBaseService(noAuth, "https://cloudant.example");
+        Request testRequest = createTestRequest(myService);
+        for (int i = 0; i < 2; i++) {
+            if (i == 1) {
+                setCustomTimeout(myService, testCases[i]);
+            }
+            RealCall testRequestCall = createServiceCall(myService, testRequest);
+            assertEquals(TimeUnit.MILLISECONDS.toSeconds(testRequestCall.getClient().readTimeoutMillis()), testCases[i]);
+        }
+    }
+
+    @Test
+    void testBasicAuthTimeout() throws NoSuchFieldException, IllegalAccessException {
+        BasicAuthenticator basicAuth = new BasicAuthenticator.Builder()
+                .username("user")
+                .password("psw")
+                .build();
+        CloudantBaseService myService = createCloudantBaseService(basicAuth, "https://cloudant.example");
+        Request testRequest = createTestRequest(myService);
+        for (int i = 0; i < 2; i++) {
+            if (i == 1) {
+                setCustomTimeout(myService, testCases[i]);
+            }
+            RealCall testRequestCall = createServiceCall(myService, testRequest);
+            assertEquals(TimeUnit.MILLISECONDS.toSeconds(testRequestCall.getClient().readTimeoutMillis()), testCases[i]);
+        }
+    }
+
+    @Test
+    void testSessionAuthTimeout() throws NoSuchFieldException, IllegalAccessException {
+        CouchDbSessionAuthenticator sessionAuth = (CouchDbSessionAuthenticator)CouchDbSessionAuthenticator.newAuthenticator("user", "pass");
+        CloudantBaseService myService = createCloudantBaseService(sessionAuth, "http://" + mockedServer.getHostName() + ":" + mockedServer.getPort());
+        Request testRequest = createTestRequest(myService);
+        for (int i = 0; i < 2; i++) {
+            setupSessionAuthRespones();
+            if (i == 1) {
+                setCustomTimeout(myService, testCases[i]);
+            }
+            RealCall testRequestCall = createServiceCall(myService, testRequest);
+            assertEquals(TimeUnit.MILLISECONDS.toSeconds(testRequestCall.getClient().readTimeoutMillis()), testCases[i]);
+
+            CouchDbSessionAuthenticator auth = (CouchDbSessionAuthenticator) myService.getAuthenticator();
+            Field clientField = auth.getClass().getDeclaredField("client");
+            clientField.setAccessible(true);
+            OkHttpClient c = (OkHttpClient) clientField.get(auth);
+            assertEquals(TimeUnit.MILLISECONDS.toSeconds(c.readTimeoutMillis()), authenticationTimeoutValue);
+        }
+    }
+
+    @Test
+    void testIAMAuthTimeout() throws NoSuchFieldException, IllegalAccessException {
         IamAuthenticator iamAuth = new IamAuthenticator.Builder()
                 .apikey("apikey")
-                .url("http://" + server.getHostName() + ":" + server.getPort())
+                .url("http://" + mockedServer.getHostName() + ":" + mockedServer.getPort())
                 .build();
-        CloudantBaseService myService = new CloudantBaseService(null, iamAuth) {
-        };
-        myService.setServiceUrl("https://cloudant.example");
-
-        // Create a server request for testing
-        HttpUrl requestUrl = HttpUrl.parse(myService.getServiceUrl()).newBuilder().addPathSegment("/").build();
-        Request req = new Request.Builder().url(requestUrl).get().build();
-
-        // Create a service call for testing
-        ServiceCall<RealCall> serviceCall = myService.createServiceCall(req, null);
-        Field callField = serviceCall.getClass().getDeclaredField("call");
-        callField.setAccessible(true);
-        RealCall requestCall = (RealCall) callField.get(serviceCall);
-        assertEquals(TimeUnit.MILLISECONDS.toSeconds(requestCall.getClient().readTimeoutMillis()), testCases[0]);
-        server.shutdown();
+        CloudantBaseService myService = createCloudantBaseService(iamAuth, "https://cloudant.example");
+        Request testRequest = createTestRequest(myService);
+        for (int i = 0; i < 2; i++) {
+            setupIamAuthResponse();
+            if (i == 1) {
+                setCustomTimeout(myService, testCases[i]);
+            }
+            RealCall testRequestCall = createServiceCall(myService, testRequest);
+            assertEquals(TimeUnit.MILLISECONDS.toSeconds(testRequestCall.getClient().readTimeoutMillis()), testCases[i]);
+        }
     }
 }
