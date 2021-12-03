38c7f3757e69000d7a6c384663135171ca5387db
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2021-12-03 09:16:16 +0000

    Update generated source code
    Generated SDK source code using:
    - Generator version 3.41.0
    - Specification version 1.0.0-dev0.0.41
    - Automation (cloudant-sdks) version 8602c1c

--- modules/cloudant/src/main/java/com/ibm/cloud/cloudant/v1/model/PostRevsDiffOptions.java
+++ modules/cloudant/src/main/java/com/ibm/cloud/cloudant/v1/model/PostRevsDiffOptions.java
@@ -118,7 +118,7 @@ public class PostRevsDiffOptions extends GenericModel {
   /**
    * Gets the documentRevisions.
    *
-   * HTTP request body for postMissingRevs and postRevsDiff.
+   * HTTP request body for operations with Document revisions.
    *
    * @return the documentRevisions
    */
