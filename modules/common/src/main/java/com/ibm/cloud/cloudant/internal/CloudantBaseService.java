cc006b2a4ca57e18755a7e500e2ce6c60044c285
Tom Blench <Thomas.Blench@ibm.com>
2022-10-19 17:09:45 +0100

    fix: add missing operation names to path validator (#351)
    * fix: add missing operation names to path validator
    
    * chore: PR feedback: fix copyright date
    
    Co-authored-by: Thomas Blench <tomblench@users.noreply.github.com>

--- modules/common/src/main/java/com/ibm/cloud/cloudant/internal/CloudantBaseService.java
+++ modules/common/src/main/java/com/ibm/cloud/cloudant/internal/CloudantBaseService.java
@@ -1,5 +1,5 @@
 /**
- * © Copyright IBM Corporation 2020, 2021. All Rights Reserved.
+ * © Copyright IBM Corporation 2020, 2022. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -108,6 +108,9 @@ public abstract class CloudantBaseService extends BaseService {
             Arrays.asList(
                 "deleteDocument",
                 "getDocument",
+                "getDocumentAsMixed",
+                "getDocumentAsRelated",
+                "getDocumentAsStream",
                 "headDocument",
                 "putDocument",
                 "deleteAttachment",
