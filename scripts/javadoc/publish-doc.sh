585774b342ef9fabe67f11fefc3ec04be84135d7
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2024-01-15 19:04:19 +0000

    fix(generated): make extend import into es6 compliant
    Generated SDK source code using:
    - Generator version 3.84.0
    - Specification version 1.0.0-dev0.1.5
    - Automation (cloudant-sdks) version 3466cbd
    
    Signed-off-by: cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>

--- scripts/javadoc/publish-doc.sh
+++ scripts/javadoc/publish-doc.sh
@@ -8,7 +8,7 @@ GIT_REPO=$(git remote get-url origin)
 
 # Create documentation
 printf ">>>>> Generate new documentation\n"
-mvn javadoc:aggregate -pl '!modules/examples'
+mvn javadoc:aggregate
 
 # Clone gh-pages branch
 printf ">>>>> Publishing javadoc for release build: repo=%s branch=%s build_num=%s job_name=%s\n" ${GIT_REPO} ${BRANCH_NAME} ${BUILD_NUMBER} ${JOB_NAME}
