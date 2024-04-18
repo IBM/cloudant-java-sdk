1b0c5647cdd47dea6802a19580b0f3ad0f17c287
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2024-04-18 10:48:22 +0000

    feat(generated): update generator to 3.89.1
    Generated SDK source code using:
    - Generator version 3.89.1
    - Specification version 1.0.0-dev0.1.6
    - Automation (cloudant-sdks) version 5dc6451
    
    Signed-off-by: cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>

--- modules/cloudant/src/main/java/com/ibm/cloud/cloudant/v1/model/SchedulerDocument.java
+++ modules/cloudant/src/main/java/com/ibm/cloud/cloudant/v1/model/SchedulerDocument.java
@@ -10,6 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
 package com.ibm.cloud.cloudant.v1.model;
 
 import java.util.Date;
