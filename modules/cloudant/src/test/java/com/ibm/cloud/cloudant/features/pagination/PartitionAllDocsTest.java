1379d605bd31a0fadeee59a6bae744bfb2476b5a
Rich Ellis <ricellis@users.noreply.github.com>
2025-05-13 17:38:56 +0100

    test: add remaining pagination operation tests

---
+++ modules/cloudant/src/test/java/com/ibm/cloud/cloudant/features/pagination/PartitionAllDocsTest.java
@@ -0,0 +1,41 @@
+/**
+ * © Copyright IBM Corporation 2025. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.ibm.cloud.cloudant.features.pagination;
+
+import com.ibm.cloud.cloudant.features.MockCloudant;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.OptionsWrapper;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.PageSupplierFactory;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.TestAllDocsResult;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.TestDocsResultRow;
+import com.ibm.cloud.cloudant.v1.model.AllDocsResult;
+import com.ibm.cloud.cloudant.v1.model.DocsResultRow;
+import com.ibm.cloud.cloudant.v1.model.PostPartitionAllDocsOptions;
+
+public class PartitionAllDocsTest extends
+    PaginationOperationTest<PostPartitionAllDocsOptions.Builder, AllDocsResult, PostPartitionAllDocsOptions, DocsResultRow> {
+
+  PartitionAllDocsTest() {
+    super(new PageSupplierFactory<AllDocsResult, DocsResultRow>(TestAllDocsResult::new, TestDocsResultRow::new,
+        true), OptionsWrapper.POST_PARTITION_ALL_DOCS.newProvider(), true);
+  }
+
+  // New Pagination
+  @Override
+  protected Pagination<PostPartitionAllDocsOptions, DocsResultRow> makeNewPagination(
+      MockCloudant<AllDocsResult> cloudant, PostPartitionAllDocsOptions options) {
+    return Pagination.newPagination(cloudant, options);
+  }
+
+}
---
+++ modules/cloudant/src/test/java/com/ibm/cloud/cloudant/features/pagination/PartitionFindTest.java
@@ -0,0 +1,41 @@
+/**
+ * © Copyright IBM Corporation 2025. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.ibm.cloud.cloudant.features.pagination;
+
+import com.ibm.cloud.cloudant.features.MockCloudant;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.OptionsWrapper;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.PageSupplierFactory;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.TestFindDocument;
+import com.ibm.cloud.cloudant.features.pagination.PaginationTestHelpers.TestFindResult;
+import com.ibm.cloud.cloudant.v1.model.Document;
+import com.ibm.cloud.cloudant.v1.model.FindResult;
+import com.ibm.cloud.cloudant.v1.model.PostPartitionFindOptions;
+
+public class PartitionFindTest extends
+    PaginationOperationTest<PostPartitionFindOptions.Builder, FindResult, PostPartitionFindOptions, Document> {
+
+    PartitionFindTest() {
+      super(new PageSupplierFactory<FindResult, Document>(TestFindResult::new, TestFindDocument::new,
+          false), OptionsWrapper.POST_PARTITION_FIND.newProvider(), false);
+    }
+
+  // New Pagination
+  @Override
+  protected Pagination<PostPartitionFindOptions, Document> makeNewPagination(
+      MockCloudant<FindResult> cloudant, PostPartitionFindOptions options) {
+    return Pagination.newPagination(cloudant, options);
+  }
+
+}
